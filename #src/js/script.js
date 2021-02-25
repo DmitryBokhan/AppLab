3cbf6e88a02fc4c644267793ddf944ae64a4b92e
Your Name <iteushm@gmail.com>
2021-02-25 23:39:49 +0300

    Залил проект в Git

---
+++ #src/js/script.js
@@ -0,0 +1,18 @@
+//@@include('alert.js')
+
+
+let button = document.getElementsByClassName("list__title");
+for (let i = 0; i < button.length; i++) {
+   button[i].addEventListener("click", function () {
+      this.classList.toggle("list_active");
+      let panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+         panel.style.maxHeight = null;
+      }
+      else {
+         panel.style.maxHeight = panel.scrollHeight + "px";
+
+      }
+
+   })
+}
